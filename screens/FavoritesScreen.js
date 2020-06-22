5809cfd3241085c7ec5cb09bb692d18700ed3951
gabrielezagarella <gabry-zaga20@hotmail.it>
2020-06-22 16:30:49 +0200

    added empty screen text to favorites

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,11 +1,21 @@
 import React from "react";
+import { View, StyleSheet } from "react-native";
 import { HeaderButtons, Item } from "react-navigation-header-buttons";
 import { useSelector } from "react-redux";
 import CustomHeaderButton from "../components/HeaderButton";
 import MealList from "../components/MealList";
+import DefaulText from "../components/DefaulText";
 
 const FavoritesScreen = props => {
   const favMeal = useSelector(state => state.meals.favoriteMeals);
+
+  if (favMeal.length === 0 || !favMeal) {
+    return (
+      <View style={styles.content}>
+        <DefaulText>No Favorites Meals Found!</DefaulText>
+      </View>
+    );
+  }
   return <MealList listData={favMeal} navigation={props.navigation} />;
 };
 
@@ -25,4 +35,12 @@ FavoritesScreen.navigationOptions = navData => {
     )
   };
 };
+
+const styles = StyleSheet.create({
+  content: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  }
+});
 export default FavoritesScreen;
